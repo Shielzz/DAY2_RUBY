66dcbc6980211679184cf3a06ae42f30c4e4c70c
Shielzz <75139987+Shielzz@users.noreply.github.com>
2021-07-06 14:26:12 +0200

    Louisa ajoute exos 3 & 5

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, quel est votre année de naissance ?"
+user_birth = gets.chomp.to_i 
+output = 2017 - user_birth
+puts "Ah ! Vous aviez donc #{output} en 2017 !"
+
---
+++ exo_05.rb
@@ -0,0 +1,7 @@
+puts "Donne moi un chiffre au hasard vite"
+number = gets.chomp.to_i
+number.times do |i| 
+	puts "salut ça fart ?"
+end
+
+
